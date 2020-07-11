6ba24df485f34a5e4aafee80c3f4c0f24338ea86
Gonchi <a20135298@pucp.pe>
2020-07-11 01:26:58 -0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import "@progress/kendo-ui";
+import "@progress/kendo-theme-default/dist/all.css";
+import { Chart, ChartInstaller } from "@progress/kendo-charts-vue-wrapper";
 
-Vue.config.productionTip = false
+Vue.use(ChartInstaller);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  components: {
+    Chart,
+  },
+  render: (h) => h(App),
+}).$mount("#app");
